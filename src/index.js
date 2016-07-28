834809fd6e8d7e2fad571b55512170d1b697a922
Ratan <ratan@Ratans-MacBook-Pro.local>
2016-07-28 16:52:50 +0530

    First Build

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@
 
 const app = require('./app');   
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3022);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 const port = app.get('port');
 const ip = app.get('ip');
 
 debugger;
-const server = app.listen(port);
+const server = app.listen(port, ip);
 
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+  console.log(`Feathers application started on ${ip}:${port}`)
 );
