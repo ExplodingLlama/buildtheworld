167a479f659c26951aa688ec89cbca39a36ca621
Ratan <ratan@Ratans-MacBook-Pro.local>
2016-08-05 15:55:35 +0530

    temporary change

--- public/app.jsx
+++ public/app.jsx
@@ -7,8 +7,8 @@ const dummyUser = {
 };
 
 // Establish a Socket.io connection
-//const socket = io();
-const socket = io.connect('https://buildtheworld-explodingllama.rhcloud.com:8443/', {'forceNew':true });
+const socket = io();
+//const socket = io.connect('https://buildtheworld-explodingllama.rhcloud.com:8443/', {'forceNew':true });
 // Initialize our Feathers client application through Socket.io
 // with hooks and authentication.
 const app = feathers()
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const app = require('./app');   
+const app = require('./app');
+const https = require('https');
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3022);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
@@ -8,8 +9,9 @@ app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 const port = app.get('port');
 const ip = app.get('ip');
 
-debugger;
-const server = app.listen(port, ip);
+const server = https.createServer({
+}, app).listen(port, ip);
+
 
 server.on('listening', () =>
   console.log(`Feathers application started on ${ip}:${port}`)
